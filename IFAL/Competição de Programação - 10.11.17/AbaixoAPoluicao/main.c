d65286b69053bada6b724f9e474e09a24555beb9
Gabriel Brasileiro <gah.lucas@gmail.com>
2017-11-11 23:43:06 -0200

    Add files via upload

---
+++ IFAL/Competição de Programação - 10.11.17/AbaixoAPoluicao/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int nc, contcar, conttotal, contcasas = 0, ntc, ntca;
+	float taxa = 12.89, total;
+	
+	while(nc != 999){
+		
+		//printf("Digite o valor de carros: \n");
+		scanf("%d", &nc);
+		
+		if(nc > 2){
+			
+			contcar = nc - 2;
+			conttotal = conttotal + contcar;
+			
+			contcasas++;
+			
+		}
+		
+	}
+	
+	ntc = conttotal - 997;
+	total = ntc * taxa;
+	ntca = contcasas - 1;
+	
+	printf("%.2f\n", total);
+	printf("%d", ntca);
+	
+	return 0;
+}
